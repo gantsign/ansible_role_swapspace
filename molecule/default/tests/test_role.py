128e6028cfa017263ce9ae1cb8faf8dcf01541fd
John Freeman <john.freeman.opensource@gmail.com>
2020-10-10 20:24:10 +0100

    Updated Molecule to 3.0.8 (#10)
    Keeping up with the latest changes.

--- molecule/default/tests/test_role.py
+++ molecule/default/tests/test_role.py
@@ -1,12 +1,3 @@
-import os
-
-import testinfra.utils.ansible_runner
-
-testinfra_hosts = testinfra.utils.ansible_runner.AnsibleRunner(
-    os.environ['MOLECULE_INVENTORY_FILE']
-).get_hosts('all')
-
-
 def test_swapspace(host):
     cmd = host.run('. /etc/profile && swapspace --version')
     assert cmd.rc == 0
